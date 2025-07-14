3dd2e9905c54501c38a412c04f0a6c1a94971c0c
Melanie Casasco <meli@MacBook-Air-de-Melanie.local>
2025-07-14 17:20:31 -0300

    add back button at born to surprise

--- app/bornToSurprise/page.tsx
+++ app/bornToSurprise/page.tsx
@@ -1,11 +1,21 @@
+'use client'
 import React from 'react'
 import Intro from './components/Intro'
 import KitsTemplate from './components/KitsTemplate'
 import kitsData from '@/app/data/bornToSurpriseKits.json'
+import { ArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 export default function BornToSurprise() {
+  const router = useRouter()
   return (
     <div>
+            <button
+        onClick={() => router.back()}
+        className="inline-flex md:text-[20px] text-[14px] items-center text-black hover:text-gray-700 transition-colors ml-4 absolute z-10 mt-6 mb-4">
+        <ArrowLeft className="mr-2 h-5 w-5" />
+        Volver
+      </button>
       <Intro />
       <div className=" mt-2 md:mt-0 mb-4">
         {kitsData.map((kit, idx) => (
